5f19ce0e693c1e442fe8f6c531aa7a36a7229a1a
Chetan Gaikwad <CGaikwad@equalexperts.com>
2019-07-19 12:30:00 +0530

    First commit

---
+++ src/main/kotlin/com/example/kotlindemo/KotlindemoApplication.kt
@@ -0,0 +1,12 @@
+package com.example.kotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlindemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlindemoApplication>(*args)
+	System.out.print("dsnbdn dsn dns dns dd,sdms")
+}
